a62056cecec6a72fa450364a31e1fd4c830e53d2
22FTT1503 <22FTT1503@student.pb.edu.bn>
2024-04-18 02:07:03 +0800

    Refactor Vite configuration and Tailwind CSS setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import purgeCSS from "@fullhuman/postcss-purgecss";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vite-project",
   plugins: [vue()],
-  css: {
-    postcss: {
-      plugins: [
-        purgeCSS({
-          content: ["./index.html", "./src/**/*.vue"],
-          safelist: ["bg-blue-500"], // Safelist any classes you use dynamically or through JavaScript
-        }),
-      ],
-    },
-  },
 });
