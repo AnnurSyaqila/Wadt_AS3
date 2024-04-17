281a2209b91263636d0358a2a0a6433ab0868215
22FTT1503 <22FTT1503@student.pb.edu.bn>
2024-04-18 01:54:02 +0800

    Update Tailwind CSS configuration to include additional content files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import purgeCSS from "@fullhuman/postcss-purgecss";
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [vue()],
+})
+  css: {
+    postcss: {
+      plugins: [
+        purgeCSS({
+          content: ["./index.html", "./src/**/*.vue"],
+          safelist: ["bg-blue-500"], // Safelist any classes you use dynamically or through JavaScript
+        }),
+      ],
+    },
+  },
 });
