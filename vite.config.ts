02fb4f676dc2c2314f5d9bab8cab0c504457f24f
22FTT1503 <22FTT1503@student.pb.edu.bn>
2024-04-18 08:15:40 +0800

    change picture to compressed version

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,8 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import purgeCSS from "@fullhuman/postcss-purgecss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Wadt_AS3/",
   plugins: [vue()],
-})
-
-
-
-css: {
-  postcss: {
-    plugins: [
-      purgeCSS({
-        content: ["./index.html", "./src/**/*.vue"],
-        safelist: ["bg-blue-500"], // Safelist any classes you use dynamically or through JavaScript
-      }),
-    ],
-  },
-},
 });
